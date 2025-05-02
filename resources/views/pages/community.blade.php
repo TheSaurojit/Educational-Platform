6597725f94104cb2f8081aab603c64fbf42afa0f
TheSaurojit <saurojitkarmakar947@gmail.com>
2025-05-02 19:43:25 +0530

    changes v2

--- resources/views/pages/community.blade.php
+++ resources/views/pages/community.blade.php
@@ -44,7 +44,6 @@
                     $time = $post?->created_at?->format('d  M , Y');
                     $name = $post?->user?->name;
                     $profile_image = $post?->user?->profile?->profile_image;
-                    dump($name , $post?->user);
                 @endphp
 
                 <div class="post-card" data-post-id="{{ $post->id }}">
